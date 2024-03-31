27a7dd48c09babdac6f5c6d385cc6687befed0fd
Mohammad Tasif Hossan <153765460+tasifhossan@users.noreply.github.com>
2024-03-31 19:15:43 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// js
+
+
+// jq
+$(document).ready(function(){
+
+    $("#ticker").eocjsNewsticker({
+
+    // animation speed
+    speed: 20,
+
+    // time to wait before starting
+    timeout: 1,
+
+    // divider between news
+    divider: '+++',
+
+    // or 'ajax'
+    type: 'static'
+    
+  });
+
+
+
+});
